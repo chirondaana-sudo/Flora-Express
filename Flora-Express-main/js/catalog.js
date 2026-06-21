7664d978180f48f96efca22cc78c5a2ec554ead2
Ana <chironda.ana@elev.cihcahul.md>
2026-06-21 22:34:55 +0300

    Actualizare site-21

--- Flora-Express-main/js/catalog.js
+++ Flora-Express-main/js/catalog.js
@@ -3,13 +3,15 @@ const products = document.querySelectorAll(".product");
 
 categoryBtns.forEach(btn => {
     btn.addEventListener("click", () => {
+
         categoryBtns.forEach(b => {
             b.classList.remove("btn-success");
             b.classList.add("btn-outline-success");
         });
 
-        b.classList.remove("btn-success");
-        b.classList.add("btn-outline-success");
+        btn.classList.remove("btn-outline-success");
+        btn.classList.add("btn-success");
+
         const category = btn.dataset.category;
 
         products.forEach(product => {
@@ -34,3 +36,25 @@ categoryBtns.forEach(btn => {
             });
 
         });
+
+const sortPrice = document.getElementById("sortPrice");
+const productList = document.getElementById("productList");
+
+sortPrice.addEventListener("change", () => {
+
+    const productsArray = Array.from(document.querySelectorAll(".product"));
+
+    productsArray.sort((a, b) => {
+
+        const priceA = Number(a.dataset.price);
+        const priceB = Number(b.dataset.price);
+
+        return sortPrice.value === "asc"
+            ? priceA - priceB
+            : priceB - priceA;
+    });
+
+    productsArray.forEach(product => {
+        productList.appendChild(product);
+    });
+});
