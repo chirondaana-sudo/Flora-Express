77e23c90feb5522a038ac49a5b199e2c4873ad1f
Ana <chironda.ana@elev.cihcahul.md>
2026-06-21 23:01:45 +0300

    Actualizare site+imagini

--- js/catalog.js
+++ js/catalog.js
@@ -1,13 +1,9 @@
-
-
 const categoryBtns = document.querySelectorAll(".category-btn");
 const products = document.querySelectorAll(".product");
 
 categoryBtns.forEach(btn => {
-
     btn.addEventListener("click", () => {
 
-        // schimbă culoarea butoanelor
         categoryBtns.forEach(b => {
             b.classList.remove("btn-success");
             b.classList.add("btn-outline-success");
@@ -16,11 +12,9 @@ categoryBtns.forEach(btn => {
         btn.classList.remove("btn-outline-success");
         btn.classList.add("btn-success");
 
-        // filtrare produse
         const category = btn.dataset.category;
 
         products.forEach(product => {
-
             if (
                 category === "all" ||
                 product.dataset.category === category
@@ -29,18 +23,38 @@ categoryBtns.forEach(btn => {
             } else {
                 product.style.display = "none";
             }
-
         });
 
     });
-
 });
 
-// buton Adaugă în coș
-const cartButtons = document.querySelectorAll(".add-cart");
+        const cartButtons = document.querySelectorAll(".add-cart");
 
-cartButtons.forEach(btn => {
-    btn.addEventListener("click", () => {
-        window.location.href = "contact.html";
+        cartButtons.forEach(btn => {
+            btn.addEventListener("click", () =>{
+                window.location.href = "contact.html";
+            });
+
+        });
+
+const sortPrice = document.getElementById("sortPrice");
+const productList = document.getElementById("productList");
+
+sortPrice.addEventListener("change", () => {
+
+    const productsArray = Array.from(document.querySelectorAll(".product"));
+
+    productsArray.sort((a, b) => {
+
+        const priceA = Number(a.dataset.price);
+        const priceB = Number(b.dataset.price);
+
+        return sortPrice.value === "asc"
+            ? priceA - priceB
+            : priceB - priceA;
+    });
+
+    productsArray.forEach(product => {
+        productList.appendChild(product);
     });
 });
--- js/validation.js
+++ js/validation.js
@@ -2,58 +2,6 @@
 VALIDARE FORMULAR CONTACT
 ========================= */
 
-document.getElementById("contactForm").addEventListener("submit", function(e) {
-
-    e.preventDefault();
-
-    const name = document.getElementById("name").value.trim();
-    const surname = document.getElementById("surname").value.trim();
-    const email = document.getElementById("email").value.trim();
-    const phone = document.getElementById("phone").value.trim();
-    const message = document.getElementById("message").value.trim();
-
-    if (name === "") {
-        alert("Introduceți numele!");
-        return;
-    }
-
-    if (surname === "") {
-        alert("Introduceți prenumele!");
-        return;
-    }
-
-    if (email === "") {
-        alert("Introduceți adresa de email!");
-        return;
-    }
-
-    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-
-    if (!emailRegex.test(email)) {
-        alert("Introduceți o adresă de email validă!");
-        return;
-    }
-
-    if (phone === "") {
-        alert("Introduceți numărul de telefon!");
-        return;
-    }
-
-    if (phone.length < 8) {
-        alert("Numărul de telefon este prea scurt!");
-        return;
-    }
-
-    if (message.length < 10) {
-        alert("Mesajul trebuie să conțină cel puțin 10 caractere!");
-        return;
-    }
-
-    alert("Mesajul a fost trimis cu succes! 🌸");
-
-    document.getElementById("contactForm").reset();
-
-});
 const form = document.getElementById("contactForm");
 
 form.addEventListener("submit", function (e) {
@@ -87,9 +35,11 @@ if (!phoneRegex.test(phone)) {
 }
 
 // succes
-alert("Mesaj trimis cu succes! Te contactăm în curând.");
+alert("Produsul a fost comandat cu succes! Un reprezentant Flora Express vă va contacta în curând.");
 
 form.reset();
-
+//window.location.href = "index.html";
 
 });
+
+
